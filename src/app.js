23b5b0f82aec0e2d74e5cfa221b79546a74227b3
Rajesh Sharma <Rajeshsharma.rs1123@gmail.com>
2020-12-26 12:00:19 +0530

    Upating to Host on Heroku

--- src/app.js
+++ src/app.js
@@ -9,7 +9,8 @@ const viewpath=path.join(__dirname,'../templates/views')
 const partialpath=path.join(__dirname,'../templates/partials')
 
 const app = express()
-const port=process.env.PORT | 3000
+
+const port = process.env.PORT || 3000
 app.set('views',viewpath)
 app.set('view engine','hbs')
 hbs.registerPartials(partialpath)
